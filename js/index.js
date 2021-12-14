0bb595ac2e9517b844895c982732a3bb6d7d7271
Ahmed Samir Morsy <ahmedsamirmorsi@hotmail.com>
2021-12-14 23:53:32 +0200

    dynamic menu

--- js/index.js
+++ js/index.js
@@ -1,9 +1,301 @@
-const chooseNav = (index, id) => {
-    console.log(index, id);   
-    document.getElementById(id).children.forEach( el => {
-        console.log(el.classList);
-        el.className = el.className.replace("active", "")
-    }) 
+const choosenMenuItems = {
+    first: "Bedingungens",
+    second: "Diavonex",
+    third: "Overview"
+}
+const menuData = {
+    "Bedingungen": {
+        "Adtralza": [
+            "Overview",
+            "Mode of Action",
+            "Efficacy",
+            "Quality of Life",
+            "Safety",
+            "Dosing",
+            "News",
+            "Technical Information",
+        ],
+        "Diavonex": [
+            "Overview",
+            "Dosing",
+            "News",
+            "Technical Information",
+        ],
+        "Diavobet": [
+            "Overview",
+            "Safety",
+            "Dosing",
+            "News",
+            "Technical Information",
+        ],
+        "Enstilar": [
+            "Dosing",
+            "News",
+            "Technical Information",
+        ],
+        "Fucidin": [
+            "Overview",
+            "Mode of Action",
+            "Dosing",
+            "News",
+            "Technical Information",
+        ],
+        "Kyntheum": [
+            "Overview",
+            "Quality of Life",
+            "Safety",
+            "Dosing",
+            "News",
+            "Technical Information",
+        ],
+        "Protopic": [
+            "Overview",
+            "Mode of Action",
+            "Safety",
+            "Dosing",
+            "News",
+            "Technical Information",
+        ],
+        "Skinoren": [
+            "Overview",
+            "Quality of Life",
+            "Safety",
+            "Dosing",
+            "News",
+            "Technical Information",
+        ],
+        "Tralokinumab": [
+            "Overview",
+            "Mode of Action",
+            "Efficacy",
+            "Quality of Life",
+        ],
+        "Xamiol": [
+            "Overview",
+            "Mode of Action",
+            "Efficacy",
+        ],
+    },
+    "Bedingungens": {
+        "Diavonex": [
+            "Overview",
+            "Dosing",
+            "News",
+            "Technical Information",
+        ],
+        "Diavobet": [
+            "Overview",
+            "Safety",
+            "Dosing",
+            "News",
+            "Technical Information",
+        ],
+        "Enstilar": [
+            "Dosing",
+            "News",
+            "Technical Information",
+        ],
+        "Fucidin": [
+            "Overview",
+            "Mode of Action",
+            "Dosing",
+            "News",
+            "Technical Information",
+        ],
+        "Kyntheum": [
+            "Overview",
+            "Quality of Life",
+            "Safety",
+            "Dosing",
+            "News",
+            "Technical Information",
+        ],
+        "Protopic": [
+            "Overview",
+            "Mode of Action",
+            "Safety",
+            "Dosing",
+            "News",
+            "Technical Information",
+        ],
+    },
+    "Veranstaltungen": {
+        "Adtralza": [
+            "Overview",
+            "Mode of Action",
+            "Efficacy",
+            "Quality of Life",
+            "Safety",
+            "Dosing",
+            "News",
+            "Technical Information",
+        ],
+        "Diavonex": [
+            "Overview",
+            "Dosing",
+            "News",
+            "Technical Information",
+        ],
+        "Diavobet": [
+            "Overview",
+            "Safety",
+            "Dosing",
+            "News",
+            "Technical Information",
+        ],
+        "Enstilar": [
+            "Dosing",
+            "News",
+            "Technical Information",
+        ],
+    },
+    "Werkzeuge": {
+
+        "Diavobet": [
+            "Overview",
+            "Safety",
+            "Dosing",
+            "News",
+            "Technical Information",
+        ],
+        "Enstilar": [
+            "Dosing",
+            "News",
+            "Technical Information",
+        ],
+        "Fucidin": [
+            "Overview",
+            "Mode of Action",
+            "Dosing",
+            "News",
+            "Technical Information",
+        ],
+        "Kyntheum": [
+            "Overview",
+            "Quality of Life",
+            "Safety",
+            "Dosing",
+            "News",
+            "Technical Information",
+        ],
+        "Protopic": [
+            "Overview",
+            "Mode of Action",
+            "Safety",
+            "Dosing",
+            "News",
+            "Technical Information",
+        ],
+        "Skinoren": [
+            "Overview",
+            "Quality of Life",
+            "Safety",
+            "Dosing",
+            "News",
+            "Technical Information",
+        ],
+        "Tralokinumab": [
+            "Overview",
+            "Mode of Action",
+            "Efficacy",
+            "Quality of Life",
+        ],
+        "Xamiol": [
+            "Overview",
+            "Mode of Action",
+            "Efficacy",
+        ],
+    },
+    "Forschung und Erkenntnisse": {
+
+        "Protopic": [
+            "Overview",
+            "Mode of Action",
+            "Safety",
+            "Dosing",
+            "News",
+            "Technical Information",
+        ],
+        "Skinoren": [
+            "Overview",
+            "Quality of Life",
+            "Safety",
+            "Dosing",
+            "News",
+            "Technical Information",
+        ],
+        "Tralokinumab": [
+            "Overview",
+            "Mode of Action",
+            "Efficacy",
+            "Quality of Life",
+        ],
+        "Xamiol": [
+            "Overview",
+            "Mode of Action",
+            "Efficacy",
+        ],
+    },
 }
 
-// document.onreadystatechange()
+const chooseNav = (name, id) => {
+    [].slice.call(document.getElementById(id).children).forEach(el => {
+        el.className = el.className.replace("active", "")
+    })
+    document.getElementById(`${id}${name}`).className += "active";
+
+    if (id == "firstNav") {
+        choosenMenuItems.first = name.split("_").join(" ");
+        settleMenuItems(true, true)
+    } else if (id == "secondNav") {
+        choosenMenuItems.second = name.split("_").join(" ");
+        settleMenuItems(false, true)
+    } else {
+        choosenMenuItems.third = name.split("_").join(" ");
+    }
+}
+
+const createMneuItem = (menuHolder, type, el, index) => {
+    const id = el.split(" ").join("_");
+    const menuItem = document.createElement("a");
+    menuItem.id = `${menuHolder}${id}`;
+    menuItem.onclick = () => {
+        chooseNav(id, `${menuHolder}`)
+    }
+    menuItem.innerHTML = el;
+    if (index == 0) {
+        choosenMenuItems[type] = el;
+        menuItem.className = "active";
+    }
+    return menuItem;
+}
+
+const settleMenuItems = (second = true, third = true) => {
+    if (second) {
+        const secondMenu = document.getElementById("secondNav");
+        const secondMenuData = menuData[choosenMenuItems.first];
+        const secondMenuDataKeys = Object.keys(secondMenuData);
+        // remove children
+        secondMenu.innerHTML = '';
+        secondMenuDataKeys.forEach((el, index) => {
+            const menuItem = createMneuItem('secondNav', "second", el, index);
+            secondMenu.append(menuItem);
+        })
+    }
+
+
+    if (third) {
+        const thirdMenu = document.getElementById("thirdNav");
+        const thirdMenuData = menuData[choosenMenuItems.first][choosenMenuItems.second];
+
+        // remove children
+        thirdMenu.innerHTML = '';
+        thirdMenuData.forEach((el, index) => {
+            const menuItem = createMneuItem('thirdNav', "third", el, index);
+            thirdMenu.append(menuItem);
+        })
+    }
+}
+
+//
+settleMenuItems();
